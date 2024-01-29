247e73d63f2b52e0a7f16015b705ba3017ed4851
abhishekbhattarai0 <abhishekbhattarai831@gmail.com>
2024-01-29 20:55:34 +0545

    commit

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -11,12 +11,12 @@ function LogoutBtn(){
         })
     }
     return(
-        <button
+        <Button
         className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
         onClick={logoutHandler}
         >
             Logout
-        </button>
+        </Button>
     )
 }
 
